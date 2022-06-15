289595755e256c79317b92f1bf348c54dca399f3
Andrey Krasutsky <AndrezzzRus@gmail.com>
2022-06-15 21:56:46 +0300

    Added files

---
+++ imageviewer.cpp
@@ -0,0 +1,40 @@
+#include "imageviewer.h"
+#include "./ui_imageviewer.h"
+
+ImageViewer::ImageViewer(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::ImageViewer)
+{
+    ui->setupUi(this);
+}
+
+ImageViewer::~ImageViewer()
+{
+    delete ui;
+}
+
+void ImageViewer::resizeEvent(QResizeEvent *event)
+{
+    QWidget::resizeEvent(event);
+    ui->imageField->setPixmap(m_image.scaled(ui->scrollArea->width(),ui->scrollArea->height(),Qt::KeepAspectRatio));
+}
+
+
+void ImageViewer::on_pathButton_clicked()
+{
+    m_imagePath = QFileDialog::getOpenFileName(this, "Choose path to your image", "../ImageViewer",
+                                             "All Files (*.*);; PNG Image (*.png);; JPEG Image (*.jpg);");
+}
+
+
+void ImageViewer::on_imageButton_clicked()
+{
+    m_image.load(m_imagePath);
+    ui->imageField->setPixmap(m_image.scaled(ui->scrollArea->width(),ui->scrollArea->height(),Qt::KeepAspectRatio));
+}
+
+
+void ImageViewer::on_exitButton_clicked()
+{
+    QMainWindow::close();
+}
---
+++ imageviewer.h
@@ -0,0 +1,33 @@
+#ifndef IMAGEVIEWER_H
+#define IMAGEVIEWER_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QPixmap>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class ImageViewer; }
+QT_END_NAMESPACE
+
+class ImageViewer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ImageViewer(QWidget *parent = nullptr);
+    ~ImageViewer();
+    void resizeEvent(QResizeEvent *event) override;
+
+private slots:
+    void on_pathButton_clicked();
+
+    void on_imageButton_clicked();
+
+    void on_exitButton_clicked();
+
+private:
+    Ui::ImageViewer *ui;
+    QString m_imagePath;
+    QPixmap m_image;
+};
+#endif // IMAGEVIEWER_H
